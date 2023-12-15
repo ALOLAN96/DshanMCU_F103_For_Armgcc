fbafad1c7cd56ec02ed6fe3f864e1f49c83baa9c
west_info <uxdascbg@outlook.com>
2023-12-15 15:44:09 +0800

    使用事件组通知汽车运行

---
+++ 19_freertos_nwatch_queueset_car_eventgroup/Core/Src/freertos.c
@@ -0,0 +1,153 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "driver_ir_receiver.h"
+#include "driver_rotary_encoder.h"
+#include "driver_mpu6050.h"
+#include "driver_lcd.h"
+#include "game1.h"
+#include "typedefs.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+SemaphoreHandle_t g_xSemOfI2CMutex;
+/* USER CODE END Variables */
+/* Definitions for startTask */
+osThreadId_t startTaskHandle;
+const osThreadAttr_t startTask_attributes = {
+    .name       = "startTask",
+    .stack_size = 128 * 4,
+    .priority   = (osPriority_t)osPriorityNormal,
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+void GetI2C(void)
+{
+    // 等待一个互斥量
+    xSemaphoreTake(g_xSemOfI2CMutex, portMAX_DELAY);
+}
+
+void PutI2C(void)
+{
+    // 释放一个互斥量
+    xSemaphoreGive(g_xSemOfI2CMutex);
+}
+/* USER CODE END FunctionPrototypes */
+
+void AppStartTask(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
+void MX_FREERTOS_Init(void)
+{
+    /* USER CODE BEGIN Init */
+    LCD_Init();
+    LCD_Clear();
+
+    IRReceiver_Init(); // 此处初始化是初始化队列
+    RotaryEncoder_Init();
+    MPU6050_Init();
+
+    extern void car_game(void *argument);
+
+    TaskHandle_t Game1TaskHandle;
+    /* USER CODE END Init */
+
+    /* USER CODE BEGIN RTOS_MUTEX */
+    g_xSemOfI2CMutex = xSemaphoreCreateMutex();
+    /* add mutexes, ... */
+    /* USER CODE END RTOS_MUTEX */
+
+    /* USER CODE BEGIN RTOS_SEMAPHORES */
+    /* add semaphores, ... */
+    /* USER CODE END RTOS_SEMAPHORES */
+
+    /* USER CODE BEGIN RTOS_TIMERS */
+    /* start timers, add new ones, ... */
+    /* USER CODE END RTOS_TIMERS */
+
+    /* USER CODE BEGIN RTOS_QUEUES */
+    /* add queues, ... */
+    /* USER CODE END RTOS_QUEUES */
+
+    /* Create the thread(s) */
+    /* creation of startTask */
+    // startTaskHandle = osThreadNew(AppStartTask, NULL, &startTask_attributes);
+
+    /* USER CODE BEGIN RTOS_THREADS */
+    xTaskCreate(car_game, "Game Task", 128, NULL, osPriorityNormal, &Game1TaskHandle);
+    /* add threads, ... */
+    /* USER CODE END RTOS_THREADS */
+
+    /* USER CODE BEGIN RTOS_EVENTS */
+    /* add events, ... */
+    /* USER CODE END RTOS_EVENTS */
+}
+
+/* USER CODE BEGIN Header_AppStartTask */
+/**
+ * @brief  Function implementing the startTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_AppStartTask */
+void AppStartTask(void *argument)
+{
+    /* USER CODE BEGIN AppStartTask */
+    /* Infinite loop */
+    for (;;) {
+        MUSIC_Analysis();
+    }
+    /* USER CODE END AppStartTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
